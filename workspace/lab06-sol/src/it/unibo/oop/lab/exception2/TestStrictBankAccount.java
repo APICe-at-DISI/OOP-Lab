c270c78d2ab08d0f32471640b34126bcfa011d76
Andrea Placuzzi <andrea.placuzzi@studio.unibo.it>
2020-10-28 18:39:57 +0100

    modify comments structure

--- workspace/lab06-sol/src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
+++ workspace/lab06-sol/src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
@@ -11,6 +11,7 @@ import org.junit.Test;
  */
 public class TestStrictBankAccount {
 
+	private static final int MAX_ATM_TRANSACTIONS = 10;
     private static final int INITIAL_AMOUNT = 10_000;
     private static final int TOO_MUCH = 50_000;
 
@@ -20,37 +21,24 @@ public class TestStrictBankAccount {
      */
     @Test
     public void testBankOperations() {
-        /*
-         * 1) Creare due StrictBankAccountImpl assegnati a due AccountHolder a
-         * scelta, con ammontare iniziale pari a 10000 e nMaxATMTransactions=10
-         */
+
         final AccountHolder usr1 = new AccountHolder("Mario", "Rossi", 1);
         final AccountHolder usr2 = new AccountHolder("Luigi", "Bianchi", 2);
-        final StrictBankAccount account1 = new StrictBankAccount(usr1.getUserID(), INITIAL_AMOUNT, 10);
-        final StrictBankAccount account2 = new StrictBankAccount(usr2.getUserID(), INITIAL_AMOUNT, 10);
-        /*
-         * 2) Effetture un numero di operazioni a piacere per verificare che le
-         * eccezioni create vengano lanciate soltanto quando opportuno, cioè in presenza
-         * di un id utente errato, oppure al superamento del numero di operazioni ATM
-         * gratuite.
-         */
+        final StrictBankAccount account1 = new StrictBankAccount(usr1.getUserID(), INITIAL_AMOUNT, MAX_ATM_TRANSACTIONS);
+        final StrictBankAccount account2 = new StrictBankAccount(usr2.getUserID(), INITIAL_AMOUNT, MAX_ATM_TRANSACTIONS);
         try {
             account1.deposit(4, 100);
             fail();
         } catch (WrongAccountHolderException e) {
             assertNotNull(e);
         }
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < MAX_ATM_TRANSACTIONS; i++) {
             try {
                 account2.depositFromATM(usr2.getUserID(), 1);
             } catch (TransactionsOverQuotaException | WrongAccountHolderException e) {
                 fail("Not exceeded yet max no. transactions!");
             }
         }
-        /*
-         * Questa istruzione genererà una eccezione di tipo
-         * TransactionsOverQuotaException
-         */
         try {
             account2.depositFromATM(usr2.getUserID(), 1);
             fail("Should raise the exception signaling that we exceeded max no. transactions!");
@@ -58,10 +46,6 @@ public class TestStrictBankAccount {
             assertNotNull(e);
         }
         try {
-            /*
-             * Questa istruzione genererà una eccezione di tipo
-             * NotEnoughFoundsException
-             */
             account1.withdraw(usr1.getUserID(), TOO_MUCH);
         } catch (WrongAccountHolderException e) {
             fail();
--- workspace/lab06/src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
+++ workspace/lab06/src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
@@ -22,19 +22,16 @@ public class TestStrictBankAccount {
     @Test
     public void testBankOperations() {
         /*
-         * 1) Creare due StrictBankAccountImpl assegnati a due AccountHolder a
-         * scelta, con ammontare iniziale pari a 10000 e nMaxATMTransactions=10
+         * TODO: uncomment this test.
+         * 
+         * You can uncomment blocks of code in eclipse by using:
+         * 
+         * Control + Shift + 7 (With an Italian keyboard...)
          */
 //        final AccountHolder usr1 = new AccountHolder("Mario", "Rossi", 1);
 //        final AccountHolder usr2 = new AccountHolder("Luigi", "Bianchi", 2);
 //        final StrictBankAccount account1 = new StrictBankAccount(usr1.getUserID(), INITIAL_AMOUNT, MAX_ATM_TRANSACTIONS);
 //        final StrictBankAccount account2 = new StrictBankAccount(usr2.getUserID(), INITIAL_AMOUNT, MAX_ATM_TRANSACTIONS);
-        /*
-         * 2) Effetture un numero di operazioni a piacere per verificare che le
-         * eccezioni create vengano lanciate soltanto quando opportuno, cioè in presenza
-         * di un id utente errato, oppure al superamento del numero di operazioni ATM
-         * gratuite.
-         */
 //        try {
 //            account1.deposit(4, 100);
 //            fail();
@@ -48,10 +45,6 @@ public class TestStrictBankAccount {
 //                fail("Not exceeded yet max no. transactions!");
 //            }
 //        }
-        /*
-         * Questa istruzione genererà una eccezione di tipo
-         * TransactionsOverQuotaException
-         */
 //        try {
 //            account2.depositFromATM(usr2.getUserID(), 1);
 //            fail("Should raise the exception signaling that we exceeded max no. transactions!");
@@ -59,10 +52,6 @@ public class TestStrictBankAccount {
 //            assertNotNull(e);
 //        }
 //        try {
-            /*
-             * Questa istruzione genererà una eccezione di tipo
-             * NotEnoughFoundsException
-             */
 //            account1.withdraw(usr1.getUserID(), TOO_MUCH);
 //        } catch (WrongAccountHolderException e) {
 //            fail();
